dc180301ba8bf0798195097f91bd6a72d543f334
mmirzafahlefi <105769503+mmirzafahlefi@users.noreply.github.com>
2023-02-09 15:22:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     response
   ) {
     document.getElementById("font-info").innerHTML = response.font;
+    document.getElementById("download-button").disabled = false;
+    document.getElementById("download-button").addEventListener("click", function () {
+      chrome.tabs.create({ url: "https://www.google.com/search?q=" + response.font + " download" });
+    });
   });
 });
